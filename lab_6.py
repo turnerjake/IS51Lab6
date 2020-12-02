6f6f15c6b688f4b2b74292525affc77bd2568a13
turnerjake <jaketurner@mail.fresnostate.edu>
2020-12-01 17:39:37 -0800

    Completed code

--- lab_6.py
+++ lab_6.py
@@ -1,20 +1,26 @@
-"""
 import random
-num = random integer
 
-if num > 15,
-    result = cherries
-if num > 10,
-    result = orange
-if num > 5,
-    result = plum
-if num > 2,
-    result = melon
-if num > 1,
-    result = bell
-else,
-    result = bar
+def main():
+    for i in range(0, 3):
+        spinWheel()
 
-loop three times
-    print(fruit) to user
-"""
+def spinWheel():
+    rand_num = random.randint(1, 20)
+    output = ""
+
+    if rand_num > 15:
+        output = "Cherries"
+    elif rand_num > 10:
+        output = "Orange"
+    elif rand_num > 5:
+        output = "Plum"
+    elif rand_num > 2:
+        output = "Melon"
+    elif rand_num > 1:
+        output = "Bell"
+    else:
+        output = "Bar"
+    
+    print(output)
+
+main()
