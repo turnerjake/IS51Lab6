2908ecd1994dbcbd42367a36e2a925c196791720
turnerjake <jaketurner@mail.fresnostate.edu>
2020-12-01 17:20:50 -0800

    Structured english

---
+++ lab_6.py
@@ -0,0 +1,12 @@
+"""
+We will generate a random number between 1 and 20
+
+if number is greater than 15 result is "cherries"
+else if number is greater than 10 result is "orange"
+else if number is greater than 5 result is "plum"
+else if number is greater than 2 result is "Melon"
+else if number is greater than 1 result is "bell"
+else result is "bar"
+
+we will iterate over a loop three times and print the result of each iteration to the user
+"""
